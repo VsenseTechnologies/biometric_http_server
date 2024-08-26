de48a14a33e117ee12aac5c25abf10e9ee9000ab
Shubhangcs <cshubhanga@gmail.com>
2024-08-26 18:01:06 +0530

    issue fixed

--- middlewares/route_middleware.go
+++ middlewares/route_middleware.go
@@ -11,7 +11,7 @@ import (
 func RouteMiddleware(authHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173/") // Set to your frontend's origin
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Set to your frontend's origin
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
