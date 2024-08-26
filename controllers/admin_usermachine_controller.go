0662f260584a9eb135f65104149a2560fe54b249
Shubhangcs <cshubhanga@gmail.com>
2024-08-26 19:33:29 +0530

    no issues

--- controllers/admin_usermachine_controller.go
+++ controllers/admin_usermachine_controller.go
@@ -8,29 +8,29 @@ import (
 	"vsensetech.in/go_fingerprint_server/payload"
 )
 
-type UserMachineController struct{
+type UserMachineController struct {
 	userMachineRepository models.UserMachineRepository
 }
 
-func NewUserMachineController(umr models.UserMachineRepository) *UserMachineController{
+func NewUserMachineController(umr models.UserMachineRepository) *UserMachineController {
 	return &UserMachineController{
 		userMachineRepository: umr,
 	}
 }
 
-func(umc *UserMachineController) FetchAllMachinesController(w http.ResponseWriter , r *http.Request){
-	data , err := umc.userMachineRepository.FetchAllMachines()
+func (umc *UserMachineController) FetchAllMachinesController(w http.ResponseWriter, r *http.Request) {
+	data, err := umc.userMachineRepository.FetchAllMachines(&r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(payload.SimpleFailedPayload{ErrorMessage: err.Error()})
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(payload.SuccessPayloadWithData{Message: "Success" , Data: data})
+	json.NewEncoder(w).Encode(payload.SuccessPayloadWithData{Message: "Success", Data: data})
 	return
 }
 
-func(umc *UserMachineController) DeleteMachineController(w http.ResponseWriter , r *http.Request) {
+func (umc *UserMachineController) DeleteMachineController(w http.ResponseWriter, r *http.Request) {
 	if err := umc.userMachineRepository.DeleteMachine(&r.Body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(payload.SimpleFailedPayload{ErrorMessage: err.Error()})
@@ -41,7 +41,7 @@ func(umc *UserMachineController) DeleteMachineController(w http.ResponseWriter ,
 	return
 }
 
-func(umc *UserMachineController) AddMachineController(w http.ResponseWriter , r *http.Request){
+func (umc *UserMachineController) AddMachineController(w http.ResponseWriter, r *http.Request) {
 	if err := umc.userMachineRepository.AddMachine(&r.Body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(payload.SimpleFailedPayload{ErrorMessage: err.Error()})
@@ -51,4 +51,3 @@ func(umc *UserMachineController) AddMachineController(w http.ResponseWriter , r 
 	json.NewEncoder(w).Encode(payload.SimpleSuccessPayload{Message: "Success"})
 	return
 }
-
