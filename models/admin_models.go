f3fdd0643c6dced337501ae0b6e17d6f728f8a54
Shubhangcs <cshubhanga@gmail.com>
2024-08-26 19:30:51 +0530

    no issues

--- models/admin_models.go
+++ models/admin_models.go
@@ -3,38 +3,38 @@ package models
 import "io"
 
 // Users Struct
-type Users struct{
+type Users struct {
 	UserName string `json:"username"`
-	UserID string `json:"user_id"`
+	UserID   string `json:"user_id"`
 }
 
 // Machines Struct
-type UserMachines struct{
+type UserMachines struct {
 	UnitID string `json:"unit_id"`
-	Status bool `json:"online"`
+	Status bool   `json:"online"`
 }
-type UserNewMachine struct{
+type UserNewMachine struct {
 	UnitID string `json:"unit_id"`
 	UserID string `json:"user_id"`
 }
 
 // Struct to Add New Users
-type ManageUsers struct{
-	Email string `json:"email"`
+type ManageUsers struct {
+	Email    string `json:"email"`
 	UserName string `json:"username"`
 	Password string `json:"password"`
 }
 
-type UsersRepository interface{
-	FetchAllUsers() ([]Users,error)
+type UsersRepository interface {
+	FetchAllUsers() ([]Users, error)
 }
 
-type UserMachineRepository interface{
-	FetchAllMachines() ([]UserMachines , error)
+type UserMachineRepository interface {
+	FetchAllMachines(*io.ReadCloser) ([]UserMachines, error)
 	DeleteMachine(*io.ReadCloser) error
 	AddMachine(*io.ReadCloser) error
 }
 
-type ManageUsersRepository interface{
+type ManageUsersRepository interface {
 	GiveUserAccess(*io.ReadCloser) error
 }
