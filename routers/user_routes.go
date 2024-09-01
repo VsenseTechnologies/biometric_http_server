e8012f6f054aae4f770f15963f9350eb5242623e
Shubhangcs <cshubhanga@gmail.com>
2024-09-01 22:38:10 +0530

    no issue

--- routers/user_routes.go
+++ routers/user_routes.go
@@ -2,7 +2,6 @@ package routers
 
 import (
 	"database/sql"
-	"net/http"
 	"sync"
 
 	"github.com/gorilla/mux"
@@ -14,14 +13,14 @@ func UserRoutes(db *sql.DB , mut *sync.Mutex , router *mux.Router){
 	repo := repository.NewFingerprintMachineRepo(db , mut)
 	cont := controllers.NewFingerprintMachineController(repo)
 
-	http.HandleFunc("/users/getmachines" , cont.FetchAllMachinesController)
+	router.HandleFunc("/users/getmachines" , cont.FetchAllMachinesController)
 
 	repos := repository.NewStudentFingerprintRepo(db , mut)
 	conts := controllers.NewStudentFingerprintController(repos)
 
-	http.HandleFunc("/users/register" , conts.RegisterStudentController)
-	http.HandleFunc("/users/fetchstudents" , conts.FetchStudentDetailsController)
-	http.HandleFunc("/users/studentslog" , conts.FetchStudentLogHistory)
-	http.HandleFunc("/users/deletestudent",conts.DeleteStudentController)
-	http.HandleFunc("/users/updatestudent" , conts.UpdateStudentController)
+	router.HandleFunc("/users/register" , conts.RegisterStudentController)
+	router.HandleFunc("/users/fetchstudents" , conts.FetchStudentDetailsController)
+	router.HandleFunc("/users/studentslog" , conts.FetchStudentLogHistory)
+	router.HandleFunc("/users/deletestudent",conts.DeleteStudentController)
+	router.HandleFunc("/users/updatestudent" , conts.UpdateStudentController)
 }
