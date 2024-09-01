32db1f8b7cbdaf544428c9ae7c4421d2ea52f41b
Shubhangcs <cshubhanga@gmail.com>
2024-09-01 22:41:20 +0530

    no issue

--- middlewares/route_middleware.go
+++ middlewares/route_middleware.go
@@ -2,11 +2,11 @@ package middlewares
 
 import (
 	"encoding/json"
-	"fmt"
+	// "fmt"
 	"net/http"
 	"strings"
 
-	"github.com/golang-jwt/jwt/v5"
+	// "github.com/golang-jwt/jwt/v5"
 	"vsensetech.in/go_fingerprint_server/payload"
 )
 
@@ -26,43 +26,43 @@ func RouteMiddleware(authHandler http.Handler) http.Handler {
 	})
 }
 
-func JwtMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var jwtSecretKey = []byte("vsense")
-		fmt.Println(r.URL.Path)
-		var path string = strings.Split(r.URL.Path, "/")[2]
-		fmt.Println(path)
-		// Bypass the middleware for login and register routes
-		if path == "login" || path == "register" {
-			next.ServeHTTP(w, r)
-			return
-		}
+// func JwtMiddleware(next http.Handler) http.Handler {
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		var jwtSecretKey = []byte("vsense")
+// 		fmt.Println(r.URL.Path)
+// 		var path string = strings.Split(r.URL.Path, "/")[2]
+// 		fmt.Println(path)
+// 		// Bypass the middleware for login and register routes
+// 		if path == "login" || path == "register" {
+// 			next.ServeHTTP(w, r)
+// 			return
+// 		}
 
-		// Get JWT token from cookies
-		cookie, err := r.Cookie("token")
-		if err != nil {
-			http.Error(w, "Unauthorized - no token provided", http.StatusUnauthorized)
-			return
-		}
+// 		// Get JWT token from cookies
+// 		cookie, err := r.Cookie("token")
+// 		if err != nil {
+// 			http.Error(w, "Unauthorized - no token provided", http.StatusUnauthorized)
+// 			return
+// 		}
 
-		tokenString := cookie.Value
-		fmt.Println(tokenString)
+// 		tokenString := cookie.Value
+// 		fmt.Println(tokenString)
 
-		// Parse and validate the token
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			// Check the signing method
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-			}
-			return jwtSecretKey, nil
-		})
+// 		// Parse and validate the token
+// 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+// 			// Check the signing method
+// 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+// 				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+// 			}
+// 			return jwtSecretKey, nil
+// 		})
 
-		if err != nil || !token.Valid {
-			http.Error(w, "Unauthorized - invalid token", http.StatusUnauthorized)
-			return
-		}
+// 		if err != nil || !token.Valid {
+// 			http.Error(w, "Unauthorized - invalid token", http.StatusUnauthorized)
+// 			return
+// 		}
 
-		// If token is valid, proceed to the next handler
-		next.ServeHTTP(w, r)
-	})
-}
+// 		// If token is valid, proceed to the next handler
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
