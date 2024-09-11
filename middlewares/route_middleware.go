ec9d1b46399ab96ced318167872b137eafb8e3b3
Srujankm12 <srujankm12@gmail.com>
2024-09-11 02:52:04 -0700

    no issue

--- middlewares/route_middleware.go
+++ middlewares/route_middleware.go
@@ -13,7 +13,7 @@ import (
 func RouteMiddleware(authHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "https://biometric-dashboard-admin.vercel.app") // Set to your frontend's origin
+		w.Header().Set("Access-Control-Allow-Origin", "https://biometric.vsensetechnologies.com") // Set to your frontend's origin
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		var url = strings.Split(r.URL.Path, "/")[1]
