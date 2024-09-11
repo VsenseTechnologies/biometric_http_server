104ea64e7eaaedcc426c2017f4d770246a269b1f
Srujankm12 <srujankm12@gmail.com>
2024-09-10 23:45:55 -0700

    no issue

--- repository/student_fingerprint_load_repository.go
+++ repository/student_fingerprint_load_repository.go
@@ -46,10 +46,10 @@ func(sfdr *StudentFingerprintDataRepo) LoadData(reader *io.ReadCloser) ([]models
 		for i , id := range reqSFDs {
 			if dbSFD.StudentID != id.StudentID {
 				dbSFDs = append(dbSFDs , dbSFD)
-				removeElement(reqSFDs , i)
+				reqSFDs = removeElement(reqSFDs , i)
 				break
 			}else{
-				removeElement(reqSFDs , i)
+				reqSFDs = removeElement(reqSFDs , i)
 			}
 		}
 	} 
