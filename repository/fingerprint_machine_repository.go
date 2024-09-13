a44b7974827c06ec59064bbb0ab7c32f01680bbc
Srujankm12 <srujankm12@gmail.com>
2024-09-13 02:35:32 -0700

    no issue

--- repository/fingerprint_machine_repository.go
+++ repository/fingerprint_machine_repository.go
@@ -73,11 +73,11 @@ func (umr *FingerprintMachineRepo) DeleteMachine(reader *io.ReadCloser) error {
     	return fmt.Errorf("unable to delete table")
 	}
 
-	if _ , err := umr.rdb.Do(umr.ctx,"JSON.DEL" , "deletes" , "$" ,machine.UnitID).Result(); err != nil {
+	if _ , err := umr.rdb.Do(umr.ctx,"JSON.DEL" , "deletes" , "$."+machine.UnitID).Result(); err != nil {
 		return err
 	}
 
-	if _ , err := umr.rdb.Do(umr.ctx,"JSON.DEL" , "inserts" , "$" ,machine.UnitID).Result(); err != nil {
+	if _ , err := umr.rdb.Do(umr.ctx,"JSON.DEL" , "inserts" , "$."+machine.UnitID).Result(); err != nil {
 		return err
 	}
 
@@ -97,11 +97,11 @@ func (umr *FingerprintMachineRepo) AddMachine(reader *io.ReadCloser) error {
 	}
 
 	
-	if _ , err := umr.rdb.Do(umr.ctx,"JSON.SET" , "deletes" , "$"+newMachine.UnitID , "[]").Result(); err != nil {
+	if _ , err := umr.rdb.Do(umr.ctx,"JSON.SET" , "deletes" , "$."+newMachine.UnitID , "[]").Result(); err != nil {
 		return err
 	}
 
-	if _ , err := umr.rdb.Do(umr.ctx,"JSON.SET" , "inserts" , "$"+newMachine.UnitID , "[]").Result(); err != nil {
+	if _ , err := umr.rdb.Do(umr.ctx,"JSON.SET" , "inserts" , "$."+newMachine.UnitID , "[]").Result(); err != nil {
 		return err
 	}
 
