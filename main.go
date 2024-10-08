546f0b7019cdd0bedcdb5f03cb2686b3aaf21087
Shubhangcs <cshubhanga@gmail.com>
2024-10-07 21:11:34 -0700

    no issues

--- main.go
+++ main.go
@@ -33,12 +33,13 @@ func main(){
 	}
 	defer db.Close()
 
+	opt , err := redis.ParseURL("redis://default:dv5AD7fFG1jLNacDyJVYSjAfwlgNQwlX@redis-16241.c305.ap-south-1-1.ec2.redns.redis-cloud.com:16241")
+
+	if err != nil {
+		log.Println(err.Error())
+	}
 	//Redis Connection
-	rdb := redis.NewClient(&redis.Options{
-		Addr: "157.230.211.229:6379",
-		Password: "",
-		DB: 0,
-	})
+	rdb := redis.NewClient(opt)
 
 	ctx := context.Background()
 	
