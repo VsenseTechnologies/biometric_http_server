d9ca7e49df9df97473084def87b06ff5b3db3372
Shubhangcs <cshubhanga@gmail.com>
2024-11-07 06:37:16 -0800

    fin

--- middlewares/route_middleware.go
+++ middlewares/route_middleware.go
@@ -15,7 +15,7 @@ func RouteMiddleware(authHandler http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "https://biometric.adminpanel.vsensetech.in") // Set to your frontend's origin
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Expose-Headers", "Set-Cookie")
 		var url = strings.Split(r.URL.Path, "/")[1]
 		if url == "admin" || url == "users" {
 			authHandler.ServeHTTP(w, r)
