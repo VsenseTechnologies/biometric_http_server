ca91c68fb856560a29cd43d3ab6f4eb64a079d6a
Shubhangcs <cshubhanga@gmail.com>
2024-11-07 06:26:22 -0800

    fin

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -28,24 +28,27 @@ func (ac *AuthController) RegisterController(w http.ResponseWriter, r *http.Requ
 		json.NewEncoder(w).Encode(payload.SimpleFailedPayload{ErrorMessage: err.Error()})
 		return
 	}
+	
+	// Set the cookie if the user is an admin
 	if urlPath == "admin" {
 		cookie := http.Cookie{
 			Name:        "token",
 			Value:       token,
-			Expires:     time.Now().Add(24 * 365 * time.Hour),
-			Secure:      true,
-			SameSite:    http.SameSiteNoneMode,
+			Expires:     time.Now().Add(24 * 365 * time.Hour), // Expires in 1 year
+			Secure:      true,  // Set to true only if you're using HTTPS
+			SameSite:    http.SameSiteStrictMode, // SameSite=None should be used if cross-site
 			Path:        "/",
 			HttpOnly:    true,
-			Partitioned: true,
+			// If needed, partition can be removed for compatibility unless specific use
+			// Partitioned: true, 
 		}
 		http.SetCookie(w, &cookie)
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(payload.SimpleSuccessPayload{Message: "Success"})
 	} else {
+		// Non-admin response with token data
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(payload.SuccessPayloadWithData{Message: "Success", Data: token})
-		return
 	}
 }
 
@@ -57,24 +60,28 @@ func (ac *AuthController) LoginController(w http.ResponseWriter, r *http.Request
 		json.NewEncoder(w).Encode(payload.SimpleFailedPayload{ErrorMessage: err.Error()})
 		return
 	}
+	
+	// Set the cookie if the user is an admin
 	if urlPath == "admin" {
 		cookie := http.Cookie{
 			Name:        "token",
 			Value:       token,
-			Expires:     time.Now().Add(24 * 365 * time.Hour),
-			Secure:      true,
-			SameSite:    http.SameSiteNoneMode,
+			Expires:     time.Now().Add(24 * 365 * time.Hour), // Expires in 1 year
+			Secure:      true,  // Set to true only if you're using HTTPS
+			SameSite:    http.SameSiteStrictMode, // SameSite=None should be used if cross-site
 			Path:        "/",
 			HttpOnly:    true,
-			Partitioned: true,
+			// If needed, partition can be removed for compatibility unless specific use
+			// Partitioned: true, 
 		}
 		http.SetCookie(w, &cookie)
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(payload.SimpleSuccessPayload{Message: "Success"})
 		return
 	} else {
+		// Non-admin response with token data
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(payload.SuccessPayloadWithData{Message: "Success", Data: token})
-		return
 	}
 }
+
