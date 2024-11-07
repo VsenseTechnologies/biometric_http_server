580be13edf1209df3967aa48f1d2f05188b711d8
Shubhangcs <cshubhanga@gmail.com>
2024-11-07 05:47:04 -0800

    cookie issue fixed

--- middlewares/route_middleware.go
+++ middlewares/route_middleware.go
@@ -15,7 +15,6 @@ func RouteMiddleware(authHandler http.Handler) http.Handler {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "https://biometric.adminpanel.vsensetech.in") // Set to your frontend's origin
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		// w.Header().Set("Access-Control-Allow-Methods", "POSR")
 		var url = strings.Split(r.URL.Path, "/")[1]
 		if url == "admin" || url == "users" {
